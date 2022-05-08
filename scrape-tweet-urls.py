931d47a7bdbea102a570e004025fb09b6f202e9a
Jaired Jawed <jairedjawed@icloud.com>
2022-05-08 16:17:10 -0700

    File Changes

--- scrape-tweet-urls.py
+++ scrape-tweet-urls.py
@@ -3,6 +3,7 @@ import ijson
 import os
 import sys
 
+'''
 class TweetURLSpider(scrapy.Spider):
   name = 'tweeturlspider'
   start_urls = ['https://google.com']
@@ -38,9 +39,36 @@ class TweetReader(object):
             # check if the tweet contains a URL
             if 'entities' in user and 'url' in user['entities']:
               print(user['id'], user['entities']['url']['urls'][0]['expanded_url'])
+'''
 
-if __name__ == '__main__':
-  numFiles = int(input('Enter the number of JSON files that have tweets within them (only in the tweets folder, they should have the format tweets_1.json, tweets_2.json, etc.): '))
 
-  reader = TweetReader(numFiles)
-  reader.streamTweets()
+
+class TweetURLScraper(scrapy.Spider):
+  name = 'tweeturlscraper'
+  fileCount = 1
+
+  def start_requests(self):
+    for i in range(self.fileCount):
+      fileExists = os.path.isfile('tweets/tweets_{}.json'.format(i+1))
+
+      if not fileExists:
+        print('The tweet file (tweets/tweets_{}.json) does not exist'.format(i+1))
+        sys.exit(0)
+
+      with open('tweets/tweets_{}.json'.format(i+1)) as f:
+        for tweet in ijson.items(f, "item"):
+          # go through all users within a tweet
+          for user in tweet['includes']['users']:
+            # check if the tweet contains a URL
+            if 'entities' in user and 'url' in user['entities']:
+              print(user['id'], user['entities']['url']['urls'][0]['expanded_url'])
+              yield scrapy.Request(user['id'], user['entities']['url']['urls'][0]['expanded_url'], self.parse)
+
+  def parse(self, response):
+     hello = response.css('title::text').get()
+     file = open('tweeturls.txt', 'a')
+     file.write(hello)
+     file.close()
+
+#if __name__ == '__main__':
+  #numFiles = int(input('Enter the number of JSON files that have tweets within them (only in the tweets folder, they should have the format tweets_1.json, tweets_2.json, etc.): '))
